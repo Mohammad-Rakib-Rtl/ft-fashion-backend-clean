7994883ad0c5a82386254b545207e79bd5b3e160
Mohammad-Rakibuzzaman <roman1117459@gmail.com>
2026-01-25 22:59:06 +0800

    add product ser up 99122112212

--- products/serializers.py
+++ products/serializers.py
@@ -10,9 +10,11 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    
     class Meta:
         model = Product
-        fields = ['id', 'category', 'name', 'description', 'price', 'image', 'code', 'created_at']
+        fields = ['id', 'category', 'category_name', 'name', 'description', 'price', 'image', 'code', 'created_at']
         read_only_fields = ['id', 'created_at']
 
     def to_representation(self, instance):
@@ -20,7 +22,22 @@ class ProductSerializer(serializers.ModelSerializer):
         
         # Ensure we return the full Cloudinary URL for the image
         if instance.image:
-            representation['image'] = instance.get_image_url()
+            # Get the actual URL
+            url = instance.image.url
+            
+            # If it's a local path, construct the Cloudinary URL
+            if url.startswith('/media/'):
+                # Try to get cloud name from environment
+                from django.conf import settings
+                cloud_name = getattr(settings, 'CLOUDINARY_CLOUD_NAME', 'di5e3wbjt')
+                filename = url.replace('/media/', '')
+                url = f"https://res.cloudinary.com/{cloud_name}/image/upload/{filename}"
+            
+            representation['image'] = url
+        
+        # Replace category ID with category name
+        if instance.category:
+            representation['category'] = instance.category.name
         
         return representation
     
