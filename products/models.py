c77a9758dd044df373079bd6b0e3d4b0c7c04e47
Mohammad-Rakibuzzaman <roman1117459@gmail.com>
2026-01-28 03:33:31 +0800

    feat: fix bugg 005

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = CloudinaryField('image')
